b56d7865e5c5f44c9cf30d699bf6664b83274d35
Томбасов Юрий Анатольевич <yuriy.tombasov@open.ru>
2018-03-26 15:14:11 +0300

    solution task 5, 6

--- 2.JavaCore/src/com/javarush/task/task17/task1706/OurPresident.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1706/OurPresident.java
@@ -3,6 +3,14 @@ package com.javarush.task.task17.task1706;
 public class OurPresident {
     private static OurPresident president;
 
+    static {
+        synchronized (president) {
+            president = new OurPresident();
+        }
+        
+    }
+    
+    
     private OurPresident() {
     }
 
