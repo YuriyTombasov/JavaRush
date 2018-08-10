3057c15dfee2e61ba3990850f744c30e4f8c8628
Томбасов Юрий Анатольевич <yuriy.tombasov@open.ru>
2018-08-10 15:47:37 +0300

    task2025 rec

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -6,10 +6,45 @@ package com.javarush.task.task20.task2025;
 public class Solution {
     public static long[] getNumbers(long N) {
         long[] result = null;
+        
+        for(long s = 1; s < N; s++){
+            int m = 1;
+            while(s / Math.pow(10, m) > 1){
+                m++;
+            }
+            
+            
+            
+            
+            
+            
+            if (s==N) break;
+        }
+        
+        
+        
         return result;
     }
+    
+    
+    public static long sum(long n, int m){
+        long mult = 1;
+        for(int i = 0; i < m; i++){
+                mult = mult * (n % 10);
+        }
+        
+        long sum = mult;
+        if (n > 9){
+            sum = sum + sum(n/10, m);
+        } 
+          
+        return sum;
+    }
+    
 
     public static void main(String[] args) {
-
+        long a = 8208;
+        System.out.println(sum(a,4));
+        
     }
 }
