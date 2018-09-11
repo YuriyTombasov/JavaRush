9a0175cb3f12318611882ea11ef1e9b5ffbca213
YuriyTombasov <u.a.tombasov@gmail.com>
2018-09-11 08:20:11 +0300

    ippodrome + snake

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -5,23 +5,85 @@
  */
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author tombasov_ya
  */
 public class Hippodrome {
+
+    private List<Horse> horses;
+
+    public static Hippodrome game;
+
+    public List<Horse> getHorses(){
+        return horses;
+    }
+
+    public Hippodrome(List<Horse> horses){
+        this.horses = horses;
+    }
     
     public static void main(String[] args){
         
+        game = new Hippodrome(new ArrayList<Horse>());
         
-        
-        
-        
+        game.horses.add(new Horse("Wight", 3, 0));
+        game.horses.add(new Horse("Black", 3, 0));
+        game.horses.add(new Horse("Brown", 3, 0));
+
+        game.run();
+
+        game.printWinner();
     }
-    
-    
-    
-    
-    
+
+    public void run(){
+
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void print(){
+        for(Horse horse : horses){
+            horse.print();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public void move(){
+        for(Horse horse : horses){
+            horse.move();
+        }
+
+    }
+
+    public Horse getWinner(){
+        Horse winner = horses.get(0);
+
+        for (Horse horse : horses){
+            if (winner.distance < horse.distance){
+                winner = horse;
+            }
+        }
+
+        return winner;
+    }
+
+    public void printWinner(){
+        System.out.println("Winner is " + getWinner().name + "!");
+    }
     
 }
--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -10,5 +10,51 @@ package com.javarush.task.task21.task2113;
  * @author tombasov_ya
  */
 public class Horse {
-    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    String name;
+    double speed, distance;
+
+    public Horse(String name, double speed, double distance){
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public void move(){
+        distance += speed * Math.random();
+
+    }
+
+    public void print(){
+        for (int i = 0; i < (int) distance; i++) {
+            System.out.print(".");
+        }
+
+        System.out.println(name);
+    }
+
 }
