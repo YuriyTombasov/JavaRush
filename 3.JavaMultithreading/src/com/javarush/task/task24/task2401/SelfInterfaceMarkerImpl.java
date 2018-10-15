ae4e99a160363184d2079659a766af6a82a4645d
Томбасов Юрий Анатольевич <yuriy.tombasov@open.ru>
2018-10-15 16:54:49 +0300

    2401 2402 resolved

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javarush.task.task24.task2401;
+
+/**
+ *
+ * @author tombasov_ya
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void p(){};
+    public void f(){};
+}
