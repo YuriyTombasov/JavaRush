e8199d3c95f9e517b2741953ef1b159934490fff
YuriyTombasov <u.a.tombasov@gmail.com>
2018-10-17 13:13:15 +0300

    new 2309

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
@@ -6,10 +6,10 @@ package com.javarush.task.task23.task2308;
 public class Solution {
     
     public final class Constants{
-        final String SERVER_IS_NOT_ACCESSIBLE_FOR_NOW = "Server is not accessible for now.";
-        final String USER_IS_NOT_AUTHORIZED = "User is not authorized."; 
-        final String USER_IS_BANNED = "User is banned."; 
-        final String ACCESS_IS_DENIED = "Access is denied."; 
+        public static final String SERVER_IS_NOT_ACCESSIBLE_FOR_NOW = "Server is not accessible for now.";
+        public static final String USER_IS_NOT_AUTHORIZED = "User is not authorized.";
+        public static final String USER_IS_BANNED = "User is banned.";
+        public static final String ACCESS_IS_DENIED = "Access is denied.";
     }
     
     public class ServerNotAccessibleException extends Exception {
