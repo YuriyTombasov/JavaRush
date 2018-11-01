7aad7b0403897871c40aca6a71e854a4e61b1a2b
Томбасов Юрий Анатольевич <yuriy.tombasov@open.ru>
2018-11-01 08:36:33 +0300

    2805

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -11,14 +11,15 @@ package com.javarush.task.task28.task2805;
  */
 public class MyThread extends Thread{
     
-    volatile int priority = Thread.MIN_PRIORITY;
+    static volatile int priority = Thread.MIN_PRIORITY;
 
     public MyThread() {
+        
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
         
     }
@@ -26,70 +27,70 @@ public class MyThread extends Thread{
     public MyThread(Runnable r) {
         super(r);
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
     }
 
     public MyThread(ThreadGroup tg, Runnable r) {
         super(tg, r);
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
     }
 
     public MyThread(String string) {
         super(string);
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
     }
 
     public MyThread(ThreadGroup tg, String string) {
         super(tg, string);
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
     }
 
     public MyThread(Runnable r, String string) {
         super(r, string);
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
     }
 
     public MyThread(ThreadGroup tg, Runnable r, String string) {
         super(tg, r, string);
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
     }
 
     public MyThread(ThreadGroup tg, Runnable r, String string, long l) {
         super(tg, r, string, l);
         setPriority(priority);
-        if(priority < Thread.MAX_PRIORITY){
+        if(priority < Thread.MAX_PRIORITY && priority < this.getThreadGroup().getMaxPriority()){
             priority = priority+1;
         } else {
-            priority = Thread.MAX_PRIORITY;
+            priority = Thread.MIN_PRIORITY;
         }
     }
     
